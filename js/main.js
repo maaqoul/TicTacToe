ebe99acc3f1e77918a01ee264bb54104db2cd1d7
maaqoul <hicham.maaqoul@gmail.com>
2020-04-21 23:52:01 +0000

    add displayController and render the table

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
 
 const GameBoardModule = (function () {
-    const board = [];
+    let board = [[], [], []];
 
     function getGameBoard() {
         return board;
     }
+
     function resetBoard() {
         board.length = 0;
     }
+
     function addMarktoBoard(row, column, mark) {
-        board[row][column] = mark;
+        board[row][column] = mark
         return board;
     }
+
     return { getGameBoard, resetBoard, addMarktoBoard }
 })()
+function handleMark(row, column) {
+    GameBoardModule.addMarktoBoard(row, column, 'x')
+}
+const displayController = (function () {
+    function addMarkToBoard() {
+        
+    }
+    function renderBoard() {
+        const table = document.querySelector('#table');
+        let boardTable = '';
 
-const displayCounter = (function () { })()
+        for (let i = 0; i < 3; i++) {
+            boardTable += `<tr>`
+            for (let j = 0; j < 3; j++) {
+                boardTable += `<td  onclick="handleMark(${i}, ${j})">
+            x
+            </td>`
+            }
+            boardTable += `</tr>`
+        }
+        table.innerHTML = boardTable;
+    }
+    return { renderBoard }
+})()
 
 
-const createPlayer = function (name) {
+
+const createPlayer = function (name, mark) {
     let score = 0;
 
     function incrementScore() {
@@ -26,16 +52,25 @@ const createPlayer = function (name) {
     }
 
     function getScore() {
-        return score
+        return score;
     }
 
     function getName() {
         return name;
+    };
+
+    function getMark() {
+        return mark;
     }
 
     return {
         getName,
+        getMark,
         getScore,
         incrementScore
     }
 }
+
+
+
+displayController.renderBoard();
