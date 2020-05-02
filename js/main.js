92db58139bd5cfcb31cc519d2065d47e95bc6fe5
maaqoul <hicham.maaqoul@gmail.com>
2020-05-02 01:07:55 +0000

    add checkforwinner method

--- js/main.js
+++ js/main.js
@@ -27,44 +27,70 @@ const playerFactory = function (name, mark) {
 }
 
 const GameBoardModule = (function () {
-    let board = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+    const board = ['', '', '', '', '', '', '', '']
 
-    function getGameBoard() {
-        return board;
+    function addToBoard(index, player) {
+        board[index] = player
     }
 
     function resetBoard() {
         board.length = 0;
     }
 
-    return { getGameBoard, resetBoard }
+    function getBoard() {
+        return board;
+    }
+
+    function getBoardLength() {
+        return board.length
+    }
+
+    return { addToBoard, resetBoard, getBoardLength, getBoard }
 })()
 
 
-
-const displayControllerModule = (function () {
-
+const playFlowModule = (function () {
     let toggleTurn = true
-    const player1 = playerFactory('hicham', 'x')
-    const player2 = playerFactory('ahmed', 'o')
-
+    const firstPlayer = playerFactory('hicham', 'x');
+    const secondPlayer = playerFactory('ahmed', 'o');
     function takeTurn() {
-        const currentPlayer = toggleTurn ? player1 : player2;
+        const currentPlayer = toggleTurn ? firstPlayer : secondPlayer;
         toggleTurn = !toggleTurn
         return currentPlayer
     }
 
-    return { takeTurn }
+    function checkForWinner() {
+        // check for vertical rows
+        for (let i = 0; i < 4; i + 3) {
+            const firstColumn = GameBoardModule.getBoard()[i];
+            const secondColumn = GameBoardModule.getBoard()[i + 1];
+            const thirdColumn = GameBoardModule.getBoard()[i + 2];
+            if (typeof firstPlayer !== '' && firstColumn === secondColumn && secondColumn === thirdColumn) {
+                console.log(firstPlayer)
+            }
+        }
+    }
+    return { takeTurn, player1: firstPlayer, player2: secondPlayer, checkForWinner }
+})()
+
+const displayControllerModule = (function () {
+
+    const columnElement = document.querySelectorAll('.column')
+
+    for (const column of columnElement) {
+        column.addEventListener('click', ({ target }) => {
+            if (target.innerText === '') {
+                const index = Number(target.dataset.pos);
+                const player = playFlowModule.takeTurn();
+                console.log(index)
+                target.innerText = player.getMark();
+                if (GameBoardModule.getBoard()[index] === undefined) {
+                    GameBoardModule.addToBoard(index, player)
+                }
+                // playFlowModule.checkForWinner()
+            }
+        })
+    }
 })()
 
 
-
-const columnElement = document.querySelectorAll('.column')
-
-for (const column of columnElement) {
-    column.addEventListener('click', ({ target }) => {
-        if (target.innerText === '') {
-            target.innerText = displayControllerModule.takeTurn().getMark();
-        }
-    })
-}
